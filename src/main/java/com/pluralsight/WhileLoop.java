4ca8ef2d92777a9a7d15fd593779cd6bdb40138a
Romell Walker <walkerromell@gmail.com>
2025-10-02 16:22:13 -0400

    finished 1st while loop exercise

---
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count=1;
+        while (count <=5) {
+            System.out.println("I love Java");
+            count = count +1;
+        }
+    }
+}
